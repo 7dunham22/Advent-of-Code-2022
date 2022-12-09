6fda1565ec673d52f580af3327f9f9ac793e9c2d
David Dunham <daviddfh@gmail.com>
2022-12-09 12:12:03 -0500

    Refactored 0902

--- 0902.py
+++ 0902.py
@@ -29,7 +29,6 @@ def moveHead(direction, distance):
     delta = deltas[direction]
     head[0] += delta[0]
     head[1] += delta[1]
-    knots[0] = head
     moveTail(1)
 
 def moveTail(knotIndex):
@@ -40,28 +39,36 @@ def moveTail(knotIndex):
   yAbsDistance = abs(head[1] - tail[1])
   xDistance = head[0] - tail[0]
   yDistance = head[1] - tail[1]
+
+  # CASE 1: Head in lateral position. Move tail 1 over in whichever direction it moved.
+  # a) vertical
   if xAbsDistance == 0 and yAbsDistance > 0:
     if tail[1] > head[1]:
       tail[1] = head[1] + 1
     else:
       tail[1] = head[1] - 1
+  # b) horizontal
   elif yAbsDistance == 0 and xAbsDistance > 0:
     if tail[0] > head[0]:
       tail[0] = head[0] + 1
     else:
       tail[0] = head[0] - 1
+  # CASE 2: Head moves into 'knight' position with abs distance [2,1] or [1,2]. Move tail to lateral position at whatever direction is at 2 abs diff.
+  # a) vertical
   elif yAbsDistance > 1 and xAbsDistance == 1:
     tail[0] += xDistance
     if yDistance > 0:
       tail[1] += 1
     else:
       tail[1] -= 1
+  # b) horizontal
   elif xAbsDistance > 1 and yAbsDistance == 1:
     tail[1] += yDistance
     if xDistance > 0:
       tail[0] += 1
     else:
       tail[0] -= 1
+  # CASE 3: While the head can only move laterally, each tail can move diagonally, so account for case in which abs diff is [2,2]. If so, move to nearest diagonal position [1,1]
   elif xAbsDistance > 1 and yAbsDistance > 1:
     if yDistance > 0:
       tail[1] += 1
@@ -71,7 +78,7 @@ def moveTail(knotIndex):
       tail[0] += 1
     else:
       tail[0] -= 1
-  knots[knotIndex] = tail
+  # Track only the last tail of the knots. Otherwise, move the next knot relative to the current knot's position.
   if knotIndex == n-1:
     visited.add(str(tail[0]) + ':' + str(tail[1]))
   else:

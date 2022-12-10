737ee9f3b24085c5012228d9a077671d922aaa3a
David Dunham <daviddfh@gmail.com>
2022-12-10 10:59:34 -0500

    1002

---
+++ 1002.py
@@ -0,0 +1,36 @@
+with open('./inputs/1001.txt') as f:
+  data = f.read().strip()
+
+commands = data.splitlines()
+n = 6
+m = 40
+
+screen = [['.' for x in range(m)] for y in range(n)]
+
+nextCycle = []
+sprite = [0,1]
+screen[0][0] = "#"
+cmdIndex = 0
+
+def moveSprite(distance):
+  for i in range(abs(distance)):
+    if distance < 0:
+      sprite[1] -= 1
+    else:
+      sprite[1] += 1
+
+for r in range(len(screen)):
+  for c in range(len(screen[0])):
+    if c == sprite[1]-1 or c == sprite[1] or c == sprite[1]+1:
+      screen[r][c] = '#'
+    cmd = commands[cmdIndex]
+    if len(nextCycle) > 0:
+      moveSprite(nextCycle.pop())
+    else:
+      if 'noop' not in cmd:
+        [_, val] = cmd.split(' ')
+        nextCycle.append(int(val))
+      cmdIndex += 1
+
+for row in screen:
+  print("".join(row))
